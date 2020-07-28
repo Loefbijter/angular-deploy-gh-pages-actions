e144260a66988e5b6ef9741a42c24ad1349e8192
dutchmartin <martijn.groeneveldt@gmail.com>
2020-07-28 20:06:55 +0200

    add debug info

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {getInput, setFailed} from '@actions/core'
 import * as commands from './commands'
-import {isFalsyVal, navigateToDirectory} from './helpers'
+import {isFalsyVal, navigateToDirectory, writeToConsole} from './helpers'
 
 async function run(): Promise<void> {
   try {
@@ -12,7 +12,7 @@ async function run(): Promise<void> {
     const buildFolder = getInput('angular_dist_build_folder')
     const angularProjectDir = getInput('angular_project_dir')
     const deployBranch = getInput('deploy_branch')
-   // const use404Fallback = getInput('use_404_fallback')
+    const use404Fallback = getInput('use_404_fallback')
 
     // if the angular project directory is not the current directory
     if (angularProjectDir !== './' && angularProjectDir !== '') {
@@ -27,9 +27,11 @@ async function run(): Promise<void> {
     })
 
     // if (use404Fallback === 'true') {
+    writeToConsole(use404Fallback)
+    writeToConsole(typeof use404Fallback)
     await commands.copyFiles(
-      `${process.cwd()}/index.html`,
-      `${process.cwd()}/404.html`
+      `${process.cwd()}/${buildFolder}/index.html`,
+      `${process.cwd()}/${buildFolder}/404.html`
     )
     // }
 
